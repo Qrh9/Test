4c4eff145685b08d9a65a24b38c3016e86f088e2
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-07 14:21:28 +0300

    Update Pyro.py

--- Pyro.py
+++ Pyro.py
@@ -19,6 +19,6 @@ api_hash = input("Enter Your API HASH : \n")
 
 with Client("RedParx", api_id=api_id, api_hash=api_hash) as bot_:
     first_name = (bot_.get_me()).first_name
-    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @ءSupports \n<code>{bot_.export_session_string()}</code>"
+    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @RedParx \n<code>{bot_.export_session_string()}</code>"
     bot_.send_message("me", string_session_)
     print(f"String Has Been Sent To Your Saved Message : {first_name}")
