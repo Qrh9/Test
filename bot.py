88f9c7670961cfc767b347795bbf036bf48c6399
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-19 02:51:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events
 api_id = '24179928'
 api_hash = 'e19c1340234edd892cee9c38bb68439c'
-bot_token = '6899343302:AAG0XQGw2-na6-JoT26dhcbFwwKGDZAieSY'
+bot_token = '6899343302:AAEFtp7G8W3AffFGum_QMGh9QSCyyYr_QFE'
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @bot.on(events.NewMessage(pattern='/start', incoming=True))
